f5d5c679d361402b09fd377b344eafa638c10beb
Jakub Berkop <jakubberkop@MacBook-Pro-Jakub.local>
2019-03-02 00:05:01 +0100

    Added GIT_HASH macro created using git hash

---
+++ getGitVersion.py
@@ -0,0 +1,9 @@
+Import("projenv")
+
+import subprocess
+
+versionHash = subprocess.check_output(["git", "rev-parse", "HEAD"])
+
+projenv.Append(CPPDEFINES=[
+    ("GIT_HASH", '\\"' + versionHash[:-1] + '\\"' )
+])
--- src/defaults.hpp
+++ src/defaults.hpp
@@ -1,6 +1,10 @@
 #pragma once
 #include <ESP8266WiFi.h>
 
+#ifndef GIT_HASH
+#define GIT_HASH "1"
+#endif
+
 namespace defaults
 {
 const char *wifi_ssid = "***REMOVED***";
@@ -8,4 +12,5 @@ const char *wifi_password = "***REMOVED***";
 IPAddress mqtt_host = IPAddress(192, 168, 0, 213);
 int mqtt_port = 1500;
 std::string deviceTopic = "hall/";
+std::string version = GIT_HASH;
 } // namespace defaults
