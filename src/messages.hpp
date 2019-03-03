47429860a2c36dc88610e965a95e5f56055cf8a1
Jakub Berkop <jakubberkop@MacBook-Pro-Jakub.local>
2019-03-03 04:04:29 +0100

    Reduced messages header

--- src/messages.hpp
+++ src/messages.hpp
@@ -1,17 +1,6 @@
 #pragma once
 
-#include <AsyncMqttClient.hpp>
-#include <ESP8266WiFi.h>
+#include <Arduino.h>
 
-void connectToWifi();
-void connectToMqtt();
-void onWifiConnect(const WiFiEventStationModeGotIP &event);
-void onWifiDisconnect(const WiFiEventStationModeDisconnected &event);
-void onMqttConnect(bool sessionPresent);
-void onMqttDisconnect(AsyncMqttClientDisconnectReason reason);
-void onMqttSubscribe(uint16_t packetId, uint8_t qos);
-void onMqttUnsubscribe(uint16_t packetId);
-void onMqttMessage(char *topic, char *payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total);
-void onMqttPublish(uint16_t packetId);
 void publishMqtt(std::string, bool state);
 void setupNetwork();
