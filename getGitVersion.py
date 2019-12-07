d9f191fe19684b06d12228eb08a9af865ca55aad
Jakub Berkop <jakubberkop@MacBook-Pro-Jakub.local>
2019-12-07 19:53:55 +0100

    Fixed git hash flag definition

--- getGitVersion.py
+++ getGitVersion.py
@@ -5,5 +5,5 @@ import subprocess
 versionHash = subprocess.check_output(["git", "rev-parse", "HEAD"])
 
 projenv.Append(CPPDEFINES=[
-    ("GIT_HASH", '\\"' + versionHash[:-1] + '\\"' )
+    ("GIT_HASH", '\\"' + str(versionHash[:-1]) + '\\"' )
 ])
