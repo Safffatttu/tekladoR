4508beea9a47afb789bf312e6e9a181e2e8fc5b5
Jakub Berkop <safffatttu@gmail.com>
2020-06-14 18:00:33 +0200

    Removed unused updateScript functions Changed hostName for updateScript

--- uploadScripts/registerUploader.py
+++ uploadScripts/registerUploader.py
@@ -9,7 +9,7 @@ Import("env")
 def on_upload(source, target, env):
     src_build_flags = env['SRC_BUILD_FLAGS']
     host_name = src_build_flags[0].split('"')[1]
-    host_name = '{}/update'.format(host_name)
+    host_name = '{}.local/update'.format(host_name)
     firmware_path = str(source[0])
     upload(firmware_path, host_name)
 
--- uploadScripts/uploadScript.py
+++ uploadScripts/uploadScript.py
@@ -1,18 +1,6 @@
 from sys import platform
 from subprocess import check_call
 
-
-def uploadByEnv(source, target, env):
-    if platform == "win32":
-        check_call(['powershell.exe', './uploadScript.ps1'])
-    else:
-        check_call(['curl', '-F', 'image=@./.pio/build/esp12e/firmware.bin',
-                    'esp8266-webupdate.local/update'])
-
-
 def upload(source, host):
     check_call(['curl', '-F', 'image=@./.pio/build/esp12e/firmware.bin',
                 host])
-    # if platform == "win32":
-    # check_call(['powershell.exe', './uploadScript.ps1', host])
-    # else:
